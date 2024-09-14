417836b72a05c8ee745e568c58547926678c241b
sahir <sahir@pop-os.localdomain>
2024-09-14 23:15:21 +0530

    chore: cors

--- src/Middlewares/cors-headers.ts
+++ src/Middlewares/cors-headers.ts
@@ -1,7 +1,13 @@
 import { NextFunction,Response,Request } from "express";
 
-export function corsHeaders(req:Request, res:Response, next:NextFunction){
-    res.header('Access-Control-Allow-Origin','http://localhost:3000,https://assignment-ai.netlify.app');
+export function corsHeaders(req: Request, res: Response, next: NextFunction) {
+    const allowedOrigins = ['http://localhost:3000', 'https://assignment-ai.netlify.app'];
+    const origin = req.headers.origin as string;
+
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
